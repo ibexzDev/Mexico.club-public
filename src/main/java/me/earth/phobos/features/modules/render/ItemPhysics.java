d73078b52cecd3bc5793fc9bfe31709ccc6440b5
ibexzDev <87093688+ibexzDev@users.noreply.github.com>
2021-12-13 22:16:24 -0500

    added itemphysics

---
+++ src/main/java/me/earth/phobos/features/modules/render/ItemPhysics.java
@@ -0,0 +1,31 @@
+package me.earth.phobos.features.modules.render;
+
+import me.earth.phobos.features.modules.*;
+import me.earth.phobos.features.setting.*;
+
+public class ItemPhysics extends Module
+{
+    public static ItemPhysics INSTANCE;
+    public final Setting<Float> Scaling;
+
+    public ItemPhysics() {
+        super("ItemPhysics",  "Apply physics to items.",  Module.Category.RENDER,  false,  false,  false);
+        this.Scaling = (Setting<Float>)this.register(new Setting("Scaling", 0.5f, 0.0f, 10.0f));
+        this.setInstance();
+    }
+
+    public static ItemPhysics getInstance() {
+        if (ItemPhysics.INSTANCE == null) {
+            ItemPhysics.INSTANCE = new ItemPhysics();
+        }
+        return ItemPhysics.INSTANCE;
+    }
+
+    private void setInstance() {
+        ItemPhysics.INSTANCE = this;
+    }
+
+    static {
+        ItemPhysics.INSTANCE = new ItemPhysics();
+    }
+}
